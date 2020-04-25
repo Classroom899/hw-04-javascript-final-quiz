ad185a6003186f477798baf23d8a0054b4edf159
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-04-25 09:15:16 -0700

    Add my array and questions

--- javascript.js
+++ javascript.js
@@ -2,11 +2,25 @@
 
 // Arrays - Loop through the arrays
 
+var questions = [
+    ["What is the file extension for when using JavaScript?", "A).xml", "B).js", "C).javascript", "D).java"],
+    ["What kind of number does Math.random() return?", "A)An integer", "B)Whole number", "C)Floating-point", "D)Nothing"],
+    ["What is JavaScript?", "A)Programming language with object-oriented capabilities", "B)A food", "C)Move script", "D)Lights"],
+];
+
+// I'll also have to add in a pos - This tells us where the user taking the quiz is within the quiz(basically a spot to have in there and tell the program)
+// Javascript starts at 0
+var pos = 0, 
+
 // mainFunction
+
+var startButton = document.getElementById("start-button")
 
 // Timer - leave this to the end and add it in because the timer is confusing
 
 // generateQuestion function
+
+
 
 // Use choices.length - So original thought with choice A, B, C, or D then use numbers of positioning for those choices
 
