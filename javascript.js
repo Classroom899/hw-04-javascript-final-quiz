1f6046b3f965ef6175a128c02d904ce1c9692f50
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-04-25 08:26:17 -0700

    Add new section with a div and test it out

---
+++ javascript.js
@@ -0,0 +1,14 @@
+// Start off by pseudo coding - You'll need to read each step carefully and make sure to add a main function to bring all your smaller pieces together
+
+// Arrays - Loop through the arrays
+
+// mainFunction
+
+// Timer - leave this to the end and add it in because the timer is confusing
+
+// generateQuestion function
+
+// Use choices.length - So original thought with choice A, B, C, or D then use numbers of positioning for those choices
+
+
+
