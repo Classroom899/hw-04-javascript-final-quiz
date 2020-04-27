f0dad1a500807f1b86581c78be886e56bc9b73a4
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-04-26 18:11:27 -0700

    Changing array of questions

--- javascript.js
+++ javascript.js
@@ -1,29 +1,44 @@
 // Start off by pseudo coding - You'll need to read each step carefully and make sure to add a main function to bring all your smaller pieces together
+var answer1 = document.getElementById("answer1");
+var answer2 = document.getElementById("answer2");
+var answer3 = document.getElementById("answer3");
+var answer4 = document.getElementById("answer4");
+
+
 
 // Arrays - Loop through the arrays
 
 function startQuiz() {
-    var questionStart = document.getElementById("startBtn"); //This (should) make the start button the element that makes the first question appear once it's hit
-        if (questionStart.style.display === "none") {
-          questionStart.style.display = "block";
-        } else {
-          questionStart.style.display = "none";
-        }
-      }
+  var questionStart = document.getElementById("startBtn"); //This (should) make the start button the element that makes the first question appear once it's hit
+  if (questionStart.style.display === "none") {
+    questionStart.style.display = "block";
+  } else {
+    questionStart.style.display = "none";
+  }
+}
 
 
-var questions = [
-    ["What is the file extension for when using JavaScript?", "A).xml", "B).js", "C).javascript", "D).java"],
-    ["What kind of number does Math.random() return?", "A)An integer", "B)Whole number", "C)Floating-point", "D)Nothing"],
-    ["What is JavaScript?", "A)Programming language with object-oriented capabilities", "B)A food", "C)Move script", "D)Lights"],
-];
+var quizQuestions {
+  var questions: [
+  {q: "What is the file extension for when using JavaScript?",options:["A).xml", "B).js", "C).javascript", "D).java"], answer:2},
+  {q: "What kind of number does Math.random() return?",options:["An integer", "B)Whole number", "C)Floating-point", "D)Nothing"],answer:3},
+  {q: "What is JavaScript?",options:["A)A programming language with object-oriented capabilities", "B)Food", "C)Movie script", "D)Lights"],answer:1},
+
+
+      
+
+    ]
+}
+
+
+
 
 function getElementById(x) {
-    return document.getElementById(x); //I'd like that universal variable for multiple elements 
+  return document.getElementById(x); //I'd like that universal variable for multiple elements 
 }
 // I'll also have to add in a pos - This tells us where the user taking the quiz is within the quiz(basically a spot to have in there and tell the program)
 // Javascript starts at 0
-var pos = 0, quiz, test, correct, question, option1, option2
+var pos = 0, quiz, test, correct, question, option1, option2, answer
 
 // mainFunction
 
@@ -47,13 +62,13 @@ function generateQuestion() {
 
 // We will need a check answer portion also 
 function checkAnswer() {
- 
+
 }
 
 
 // Put my questions into an array to use to go through the questions and I want to add a radio feature
 question = questions[pos][0]; //Arrays start at zero so I need question to be equal to questions array at the starting position using zero
 option1 = questions[pos][1]; //Keep to this format and keep going on it -  1 because it goes from 0 to 1
-option2= questions[pos][2]; //Ends with 2 because I only have 3 questions in total
+option2 = questions[pos][2]; //Ends with 2 because I only have 3 questions in total
 test.innerHTML
 
