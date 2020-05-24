2a2f8f5bbb0b1056dc0d28e5db1d64d6c89ddf59
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-05-23 21:56:31 -0700

    Updated var quizQuestions to the correct format and correct syntax

--- javascript.js
+++ javascript.js
@@ -7,9 +7,38 @@ var answer2 = document.getElementById("answer2");
 var answer3 = document.getElementById("answer3");
 var answer4 = document.getElementById("answer4");
 
+var quizQuestions = [
+   
+  { q: 'What is a string?', options: ['A: Any grouping of characters surrounded by single or double quotes', 'B: A place', 'C: An inner HTML', 'D: A new function'], answer: 1 },
+  { q: 'What kind of number does Math.random return?', options: ['A: An integer', 'B: A floating point', 'C: A whole number', 'D: Nothing'], answer: 2 },
+  { q: 'What does null represent?', options: ['A: A new game coming out', 'The intentional absence of value', 'C: A new idea', 'D: An SQL element'], answer: 2 },
+  
+]
+
+var person = { first: 'Joe', last: 'Santos', likesMovies: true, age: 22 };
+var cars = ['BMW', 'VW', "Ferrari"];
+
+// Will need the start button here in order to startQuiz
+
 
 
 // Arrays - Loop through the arrays
+
+function main() {
+  var questionStart = quizQuestions[i]; //Make sure to declare this at the top
+  questionTitle.innerText = individualQuestion.question;
+
+  selectionList.innerHTML = ""; //reset choices list
+  for (key in individualQuestion.choices) {
+    var radioBtnName = "question" + i + "_choice";
+    var choiceText = individualQuestion.choices[key];
+    selectionList.appendChild(createLi(radioBtnName, choiceText));
+  }
+}
+
+
+
+
 
 function startQuiz() {
   var questionStart = document.getElementById("startBtn"); //This (should) make the start button the element that makes the first question appear once it's hit
@@ -19,55 +48,25 @@ function startQuiz() {
     questionStart.style.display = "none";
   }
 
+  //Will need something to grab the first question
 
 
-var questionStart {
-  var questions: [
-                  {q: "What is the file extension for when using JavaScript?",options:["A).xml", "B).js", "C).javascript", "D).java"], answer:2},
-                  {q: "What kind of number does Math.random() return?",options:["An integer", "B)Whole number", "C)Floating-point", "D)Nothing"],answer:3},
-                  {q: "What is JavaScript?",options:["A)A programming language with object-oriented capabilities", "B)Food", "C)Movie script", "D)Lights"],answer:1},
+  questionBox.innerHTML = question.questions[question.index].q;
+  answer1.innerHTML = question.questions[questions.index].options[0];
+  answer2.innerHTML = question.questions[questions.index].options[1];
+  answer3.innerHTML = question.questions[questions.index].options[2];
+  answer4.innerHTML = question.questions[questions.index].options[3];
 
 
-      
 
-    ]
-      document.addEventListener("click", startQuiz() {
-      document.getElementById("startBtn").innerHTML = "Start Quiz";
-    });
-
-    questionBox.innerHTML=question.questions[question.index].q;
-    answer1.innerHTML=question.questions[questions.index].options[0];
-    answer2.innerHTML=question.questions[questions.index].options[1];
-    answer3.innerHTML=question.questions[questions.index].options[2];
-    answer4.innerHTML=question.questions[questions.index].options[3];
-
-    
-
-    function generateQuestion() {
-      var question = getElementById("question");
-      if (pos >= questions.length) {
-        questionContainer.innerHTML = "<h2  " + correct + "  of  " + questions.length + "  questions correct<h2>";
-      }
+  function generateQuestion() {
+    var question = getElementById("question");
+    if (pos >= questions.length) {
+      questionContainer.innerHTML = "<h2  " + correct + "  of  " + questions.length + "  questions correct<h2>";
     }
+  }
 
 };
-}
-    
-
-  
-    
-    
-    
-
-
-  
-
-
-
-// Put my questions into an array to use to go through the questions
-//Arrays start at zero so I need question to be equal to questions array at the starting position using zero
-//Keep to this format and keep going on it -  1 because it goes from 0 to 1
-//Ends with 2 because I only have 3 questions in total
 
 
 
@@ -76,7 +75,7 @@ var questionStart {
 //I'd like that universal variable for multiple elements 
 // I'll also have to add in a pos - This tells us where the user taking the quiz is within the quiz(basically a spot to have in there and tell the program)
 // Javascript starts at 0
-var pos = 0, quiz, test, correct, question, option1, option2, answer, 
+var pos = 0, quiz, test, correct, question, option1, option2, answer,
 
 // mainFunction
 
@@ -87,9 +86,32 @@ var startButton = document.getElementById("startBtn")
 // generateQuestion function
 
 
-document.addEventListener("click", function(){
-  document.getElementById("answer1").innerHTML = "Hello World";
+document.addEventListener("click", function () {
+  document.getElementById("answer1").innerHTML = "";
   console.log("answer1")
+
 });
+
+const button = document
+  .getElementById('answer1')
+  .addEventListener('click', buttonClick);
+console.log(button);
+
+function buttonClick(e) {
+  console.log('Button clicked');
+  document.getElementById('header-title').textContent = 'Changed';
+  document.querySelector('#main').style.backgroundColor = '#f4f4f4';
+  console.log(e);
+
+  console.log(e.target);
+  console.log(e.target.id);
+  console.log(e.target.className);
+  console.log(e.target.classList);
+  const output = document.getElementById('output');
+  output.innerHTML = `<h3>${e.target.id}</h3>`;
+
+
+}
+
 
 
---
+++ questions.js
@@ -0,0 +1,29 @@
+var questions = [{
+    "question": "What is a string?",
+    "answer": [
+        "A: Any grouping of characters surrounded by single or double quotes",
+        "B: A place",
+        "C: An inner HTML element",
+        "D: A new function",
+    ]
+},
+{
+    "question": "What does null represent?",
+    "answer": [
+        "A: A new game coming out",
+        "B: The intentional absence of value",
+        "C: A new idea",
+        "D: An SQL element",
+
+    ]
+},
+{
+    "question": "What kind of number does Math.random return?",
+    "answer": [
+        "A: An integer",
+        "B: floating-point",
+        "C: A whole number",
+        "D: Nothing",
+    ]
+}
+];
